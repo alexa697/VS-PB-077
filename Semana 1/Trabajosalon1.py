7c1f79faa838aac6ec384c6bf68692f1be8af34c
alexa697 <alexa.murillos@uanl.edu.mx>
2025-01-24 13:55:02 -0600

    m

---
+++ Semana 1/Trabajosalon1.py
@@ -0,0 +1,19 @@
+print('actividades salón')
+v1=5
+if v1 < 4:
+    print ('dentro del if')
+    print ('aún dentro')
+print ('Ya me salí')
+
+#Actividad 1
+print ('Hola mundo')
+
+#Actividad 2
+
+a=int(input("ingrese el primer número: "))
+b=int(input("ingrese el primer número: "))
+
+print("adición = ",a + b)
+print("sustracción = ",a - b)
+print("multiplicación = ",a * b)
+print("división = ",a / b)
