604c8670df9d76bb3bc1110641c2cbcae83aae08
alexa697 <alexa.murillos@uanl.edu.mx>
2025-01-24 19:20:57 -0600

    VS-PB-077

--- Actividades/Ejercicios.py
+++ Actividades/Ejercicios.py
@@ -1,10 +1,3 @@
-print('actividades salón')
-v1=5
-if v1 < 4:
-    print ('dentro del if')
-    print ('aún dentro')
-print ('Ya me salí')
-
 #Actividad 1
 print ('Hola mundo')
 
@@ -17,3 +10,5 @@ print("adición = ",a + b)
 print("sustracción = ",a - b)
 print("multiplicación = ",a * b)
 print("división = ",a / b)
+
+#Actividad 3
