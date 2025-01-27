f41d66b0325c1e11c07c2e826b04d1b1b3f03d6a
alexa697 <alexa.murillos@uanl.edu.mx>
2025-01-26 23:29:28 -0600

    VS-PB-077

---
+++ Actividades/Act3.py
@@ -0,0 +1,76 @@
+#Hola
+print("lABORATORIO SEMANA 1 Programación Básica")
+#Instrucciones
+#Responde las siguientes preguntas y ejercicios relacionados con los conceptos b´asicos de programaci´on. Aseg´urate de justificar tus respuestas cuando sea necesario y seguir el formato indicado.
+print('Ejercicios')
+print(' ')
+print('Ejercicio 1')
+print('¿Cuál es la función de la memoria secundaria en una computadora?')
+#a) Ejecutar todos los c´alculos y la l´ogica del programa.
+#b) Descargar p´aginas web de Internet.
+#c) Almacenar informaci´on durante mucho tiempo, incluso despu´es de ciclos de apagado
+#y encendido.
+#d) Recolectar la entrada del usuario.
+print('c) Almacenar informaci´on durante mucho tiempo, incluso despu´es de ciclos de apagado y encendido')
+print(' ')
+print('Ejercicio 2')
+print('¿Qué es un programa?')
+print('Un programa es un conjunto de instrucciones que le decimos a una computadora para que realice una tarea específica.')
+print(' ')
+print('Ejercicio 3')
+print('¿Cuál es la diferencia entre un compilador y un interprete')
+print('el compilador traduce todo el código de una vez, genera un archivo ejecutable y es más rápido, y el intérprete traduce y ejecuta por línea, no genera un archivo ejecutable y es más flexible para desarrollo.')
+print(' ')
+print('Ejercicio 4')
+print('¿Cuál de los siguientes contiene “código máquina”?')
+#a) El int´erprete de Python.
+#b) El teclado.
+#c) El c´odigo fuente de Python.
+#d) Un documento de un procesador de texto.
+print('a) el interprete de phyton')
+print(' ')
+print('Ejercicio 5')
+print('¿Qué está mal en el siguiente código?')
+#>>> primt ’¡Hola, mundo!’
+#File "<stdin>", line 1
+#primt ’¡Hola, mundo!’
+#^
+#SyntaxError: invalid syntax
+#>>>
+print('El problema está en la palabra primt. En Python, la función para imprimir texto en la consola se escribe como print, no primt')
+print(' ')
+print('Ejercicio 6')
+print('¿En qué lugar del computador queda almacenada una variable, como en este caso “x”,después de ejecutar la siguiente línea de Python?:x = 123')
+#a) Unidad central de procesamiento.
+#b) Memoria Principal.
+#c) Memoria Secundaria.
+#d) Dispositivos de Entrada.
+#e) Dispositivos de Salida
+print('a) unidad central de procesamiento')
+print(' ')
+print('Ejercicio 7')
+print('¿Qu´e mostrar´a en pantalla el siguiente programa?:')
+print('x=43')
+print('x=x+1')
+print('print(x)')
+#a) 43.
+#b) 44.
+#c) x + 1.
+#d) Error, porque x = x + 1 no es posible matem´aticamente.
+print('b) 44.')
+print(' ')
+print('Ejercicio 8')
+print('Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:')
+print('1. Unidad Central de Procesamiento (CPU): Analogía: El cerebro humano.')
+print('Explicación: La CPU es el cerebro de la computadora. Al igual que nuestro cerebro procesa la información que recibimos a través de los sentidos, toma decisiones y controla nuestras acciones, la CPU ejecuta las instrucciones de los programas, realiza cálculos y coordina el funcionamiento de todos los componentes de la computadora.')
+print('2. Memoria principal: Analogía: La memoria a corto plazo.')
+print('Explicación: La memoria principal (RAM) es como la memoria a corto plazo de una persona. Al igual que recordamos temporalmente un número de teléfono mientras lo marcamos, la RAM almacena los datos y programas que la CPU está utilizando en ese momento. Sin embargo, cuando apagamos la computadora (o dejamos de pensar en el número), esta información se pierde.')
+print('3. Memoria secundaria: Analogía: La memoria a largo plazo.')
+print('Explicación: La memoria secundaria (disco duro, SSD) es como la memoria a largo plazo. Al igual que recordamos eventos importantes de nuestra vida, la memoria secundaria almacena de forma permanente la información, como documentos, fotos, música y programas.')
+print('4. Dispositivos de entrada: Analogía: Los sentidos.')
+print('Explicación: Los dispositivos de entrada son como nuestros sentidos. Nos permiten introducir información en la computadora.')
+print(' 5. Dispositivos de salida: Analogía: Los músculos y las cuerdas vocales.')
+print('Explicación: Los dispositivos de salida nos permiten obtener información de la computadora.')
+print(' ')
+print('Ejercicio 9')
+print(' ')
